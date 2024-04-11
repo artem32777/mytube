33823221884ffa8123bc426e2ae0da69f73e466c
Artem Loshinin <109870624+artem32777@users.noreply.github.com>
2024-04-12 02:24:18 +0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.VITE_SOME_KEY === 'production' ? '/mytube/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/mytube/' : '/',
   plugins: [
     vue(),
   ],
